eb5273c2887fb2acfac7cf0859237a58e3b3afde
Jihye Park <deviloper.park@gmail.com>
2024-07-04 16:10:19 +0900

    Initial Commit

---
+++ SearchLocation/SearchLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchLocationApp.swift
+//  SearchLocation
+//
+//  Created by 박지혜 on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
