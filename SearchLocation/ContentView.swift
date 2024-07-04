388adfe8792ddf4acfcc23bc20c603aaa83fdfea
Jihye Park <deviloper.park@gmail.com>
2024-07-04 16:39:15 +0900

    feat: LocationManager 생성 및 위치정보 업데이트

--- SearchLocation/ContentView.swift
+++ SearchLocation/ContentView.swift
@@ -14,28 +14,42 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            Map(position: $viewModel.cameraPosition)
-                .mapStyle(viewModel.mapStyle)
-                .navigationTitle("SearchLocation")
-                .searchable(text: $viewModel.searchText)
-                .searchScopes($style) {
-                    Text("Standard").tag(0)
-                    Text("Imagery").tag(1)
-                    Text("Hybrid").tag(2)
-                }
-                // searchScopes 적용
-                .onChange(of: style) {
-                    switch style {
-                    case 0:
-                        viewModel.mapStyle = .standard
-                    case 1:
-                        viewModel.mapStyle = .imagery
-                    case 2:
-                        viewModel.mapStyle = .hybrid
-                    default:
-                        viewModel.mapStyle = .standard
+            ZStack(alignment: .bottom) {
+                Map(position: $viewModel.cameraPosition)
+                    .mapStyle(viewModel.mapStyle)
+                    .navigationTitle("SearchLocation")
+                    .searchable(text: $viewModel.searchText)
+                    .searchScopes($style) {
+                        Text("Standard").tag(0)
+                        Text("Imagery").tag(1)
+                        Text("Hybrid").tag(2)
                     }
+                    // searchScopes 적용
+                    .onChange(of: style) {
+                        switch style {
+                        case 0:
+                            viewModel.mapStyle = .standard
+                        case 1:
+                            viewModel.mapStyle = .imagery
+                        case 2:
+                            viewModel.mapStyle = .hybrid
+                        default:
+                            viewModel.mapStyle = .standard
+                        }
+                    }
+                HStack {
+                    Button(action: {
+                        viewModel.moveToCurrentLocation()
+                    }) {
+                        Image(systemName: "location.fill")
+                            .padding()
+                            .background(.white)
+                            .clipShape(Circle())
+                    }
+                    Spacer()
                 }
+                .padding()
+            }
         }
     }
 }
--- SearchLocation/MapViewModel.swift
+++ SearchLocation/MapViewModel.swift
@@ -9,8 +9,30 @@ import SwiftUI
 import MapKit
 
 @Observable
-class MapViewModel {
+class MapViewModel: NSObject, CLLocationManagerDelegate {
+    // 위치 권한 허용하면 현재 위치 계속 업데이트
     var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
     var searchText = ""
     var mapStyle: MapStyle = .standard
+    
+    private var locationManager: CLLocationManager = CLLocationManager()
+    
+    override init() {
+        super.init() /// NSObject
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func moveToCurrentLocation() {
+        cameraPosition = .userLocation(fallback: .automatic)
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Error: \(error.localizedDescription)")
+    }
 }
